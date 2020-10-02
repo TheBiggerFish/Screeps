79b08cc4865066ecd3cfe11380c65777a969e6e5
TheBiggerFish <clhnumber4@gmail.com>
2020-10-02 11:15:55 -0400

    Moved memory management out of main loop

--- main.js
+++ main.js
@@ -3,15 +3,9 @@ module.exports.loop = function () {
     var factory = require('factory');
     factory.produce()
 
-    //Roles: https://jsonformatter.curiousconcept.com
-    Memory.ROLES = {"ROLE_PRIORITIES":["HARVESTER","UPGRADER","BUILDER","TRANSPORTER"],"ROLES":{"HARVESTER":{"name":"harvester","count":8,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"UPGRADER":{"name":"upgrader","count":6,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"BUILDER":{"name":"builder","count":4,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"TRANSPORTER":{"name":"transporter","count":0,"loadouts":[[MOVE,MOVE,MOVE,CARRY,CARRY,CARRY]]}}}
-    Memory.NUMERICS.CREEP_COUNT = Object.keys(Game.creeps).length;
-    Memory.NUMERICS.ROLES = Object.keys(Memory.ROLES.ROLES).length;
+    doMemoryConstants()
 
-    for(var i = Memory.NUMERICS.ROLES; i > 0; i--) {
-        var role = Memory.ROLES.ROLE_PRIORITIES[i-1]
-        factory.maintainRole(Memory.ROLES.ROLES[role])
-    }
+
     for(var key in Game.creeps) {
         var creep = Game.creeps[key]
         if (creep.memory.role == 'harvester') {
@@ -28,3 +22,17 @@ module.exports.loop = function () {
         }
     }
 };
+
+
+function doMemoryConstants() {
+    //Roles: https://jsonformatter.curiousconcept.com
+    Memory.ROLES = {"ROLE_PRIORITIES":["HARVESTER","UPGRADER","BUILDER","TRANSPORTER"],"ROLES":{"HARVESTER":{"name":"harvester","count":8,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"UPGRADER":{"name":"upgrader","count":10,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"BUILDER":{"name":"builder","count":2,"loadouts":[[MOVE,MOVE,WORK,CARRY],[MOVE,WORK,WORK,CARRY]]},"TRANSPORTER":{"name":"transporter","count":0,"loadouts":[[MOVE,MOVE,MOVE,CARRY,CARRY,CARRY]]}}}
+    Memory.NUMERICS.CREEP_COUNT = Object.keys(Game.creeps).length;
+    Memory.NUMERICS.ROLES = Object.keys(Memory.ROLES.ROLES).length;
+
+    var expectedCreeps = 0
+    for(var roleName in Memory.ROLES.ROLES) {
+        expectedCreeps += Memory.ROLES.ROLES[roleName].count
+    }
+    Memory.NUMERICS.CREEP_COUNT_EXPECTED = expectedCreeps
+}
