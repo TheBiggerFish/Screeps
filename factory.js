4595ff6d7cdcad3e9d650fb8f72c1a6ed8bfcdb9
TheBiggerFish <clhnumber4@gmail.com>
2020-10-02 11:16:13 -0400

    Implemented factory production

--- factory.js
+++ factory.js
@@ -8,24 +8,86 @@
  */
 
 
+
 module.exports.produce = function() {
-    for(var spawn in Game.spawns) {
-        Game.spawns[spawn].memory;
-        if(!Object.keys(Memory.spawns[spawn]).includes("NEXT_SPAWN")){
-            Game.spawns[spawn].memory.NEXT_SPAWN = {"COST":-1,"NAME":""}
+    for(var spawnName in Game.spawns) {
+        var spawn = Game.spawns[spawnName]
+        if(spawn.spawning != null || Memory.NUMERICS.CREEP_COUNT >= Memory.NUMERICS.CREEP_COUNT_EXPECTED)
+            return
+
+        spawn.memory
+        if(!Object.keys(spawn).includes("NEXT_SPAWN")){
+            spawn.memory.NEXT_SPAWN = {"COST":-1,"ROLE":"","NAME":""}
+        }
+
+        if(spawn.memory.NEXT_SPAWN.COST == -1) {
+            spawn.memory.NEXT_SPAWN = this.findNext();
         }
         
-        //console.log(Object.keys(Memory.spawns.Spawn1.NEXT_SPAWN).includes('COST'))
+        if(spawn.memory.NEXT_SPAWN.COST > -1 && spawn.memory.NEXT_SPAWN.COST <= spawn.room.energyAvailable) {
+            var x = this.create(spawn,spawn.memory.NEXT_SPAWN.ROLE,spawn.memory.NEXT_SPAWN.NAME)
+            spawn.memory.NEXT_SPAWN = this.findNext();
+        }
     }
-};
-
-module.exports.maintainRole = function(role) {
-    for (var i = role.count; i > 0; i--) {
-        var worker_name = role.name + i.toString()
-        var rv = Game.spawns.Spawn1.spawnCreep(role.loadouts[0],worker_name,{memory:{'role':role.name,'working':true}})
-        //console.log(worker_name,rv)
-        if(rv == OK)
-          console.log('Creating a new', role.name)
-    }    
 }
 
+
+module.exports.determineCost = function(loadout) {
+    var cost = 0
+    for(var partNum in loadout) {
+        cost += BODYPART_COST[loadout[partNum]]
+    }
+    return cost
+}
+
+
+module.exports.findNext = function() {
+    for(var i = 0; i < Memory.NUMERICS.ROLES; i++) {
+        var roleName = Memory.ROLES.ROLE_PRIORITIES[i]
+        var role = Memory.ROLES.ROLES[roleName]
+
+        var build = false
+        var worker_name
+        for(var j = 1; j <= role.count; j++) {
+            worker_name = role.name + j.toString()
+            var rv = this.create(Game.spawns.Spawn1,roleName,worker_name,true)
+            if(rv == OK) {
+                build = true
+                break
+            }
+        }
+
+        if(build) {
+            var cost = this.determineCost(role.loadouts[0])
+            console.log("Found next creep to build:",worker_name)
+            return {"COST":cost,"ROLE":roleName,"NAME":worker_name}
+        }
+    }
+    return {"COST":-1,"ROLE":"","NAME":""}
+}
+
+
+module.exports.create = function(spawn,roleName,name,dry=false) {
+    var role = Memory.ROLES.ROLES[roleName]
+    var rv = spawn.spawnCreep(role.loadouts[0],name,{"dryRun":dry,memory:{'role':role.name,'working':true}})
+    if(rv != OK && rv != ERR_NAME_EXISTS)
+        console.log("Failed to create creep:",Memory.ERRORS[rv.toString()],"\n\t",spawn.name,roleName,name,dry)
+    return rv
+}
+
+
+
+module.exports.maintainRole = function(roleName) {
+    var creating = false
+    var role = Memory.ROLES.ROLES[roleName]
+    for (var i = role.count; i > 0; i--) {
+        var worker_name = role.name + i.toString()
+        if(this.create(Game.spawns.Spawn1,roleName,worker_name) == OK)
+            creating = true
+    }  
+    if(creating) {
+        console.log('Creating a new', role.name)
+        return true
+    }
+    return false
+}
